2e4bc250cfe9198787469c4954e4377aa88ffb3f
Wasabicat233 <3211369024@qq.com>
2026-04-06 12:32:36 +0800

    5

--- readers_writers_starter.py
+++ readers_writers_starter.py
@@ -172,6 +172,8 @@ def main() -> None:
     #TODO: Create at least 3 Reader threads.
     readers = [
         Reader(reader_id=1, monitor=monitor)
+        Reader(reader_id=2, monitor=monitor)
+        Reader(reader_id=3, monitor=monitor)
     ]
     
     #TODO: Create at least 2 writer threads.
